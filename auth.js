0b0aee390302dbb17dc3f370316c17a8e8b64cce
RajeshChristopher <47190431+RajeshChristopher@users.noreply.github.com>
2022-11-21 01:41:19 +0530

    passport github strategy auth

--- auth.js
+++ auth.js
@@ -9,6 +9,8 @@ const ObjectID = require("mongodb").ObjectID;
 
 const LocalStrategy = require("passport-local");
 
+const GitHubStrategy = require('passport-github').Strategy;
+
 module.exports = function( app, myDataBase){
 
     passport.serializeUser(function(user,done){
@@ -39,5 +41,42 @@ passport.use(new LocalStrategy(function(username, password, done){
     return done(null,user);
   });
 }));
+
+  passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: "https://Advanced-Node-Freecodcamp.rajeshchristoph.repl.co/auth/github/callback"
+},
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    //Database logic here with callback containing our user object
+    myDataBase.findOneAndUpdate(
+  { id: profile.id },
+  {
+    $setOnInsert: {
+      id: profile.id,
+      username: profile.username,
+      name: profile.displayName || 'John Doe',
+      photo: profile.photos[0].value || '',
+      email: Array.isArray(profile.emails)
+        ? profile.emails[0].value
+        : 'No public email',
+      created_on: new Date(),
+      provider: profile.provider || ''
+    },
+    $set: {
+      last_login: new Date()
+    },
+    $inc: {
+      login_count: 1
+    }
+  },
+  { upsert: true, new: true },
+  (err, doc) => {
+    return cb(null, doc.value);
+  }
+);
+  }
+));
   
 }
