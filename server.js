a1512930a1deb5f727757095fb839599da64a3c0
RajeshChristopher <47190431+RajeshChristopher@users.noreply.github.com>
2022-11-21 01:38:33 +0530

    Socket http dependency

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const auth = require("./auth.js");
 
 const app = express();
 
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 app.set("view engine","pug");
+//app.set('views', './views/pug');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -42,6 +46,10 @@ myDB(async function (client){
   routes(app, myDataBase);
 
   auth(app, myDataBase)
+
+  io.on('connection', socket => {
+  console.log('A user has connected');
+});
 
 
 
@@ -84,6 +92,6 @@ myDB(async function (client){
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
